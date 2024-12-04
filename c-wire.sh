4ac5b43cbb375666d4b0b0ecefb6fd7df1d21ded
Strasox <133694962+Strasox@users.noreply.github.com>
2024-12-04 14:45:16 +0000

    Ajout dans le shell

--- c-wire.sh
+++ c-wire.sh
@@ -2,6 +2,34 @@
 
 ## On verifie les argument entrer par l'utilisateur
 
+# Fonction pour afficher l'aide (-h)
+affiche_-h(){
+    echo "Usage : $0 [options] [paramètres]"
+    echo
+    echo "Description :"
+    echo "Ce script permet de traiter les informations des stations et des centrales."
+    echo
+    echo "Options :"
+    echo "  -h        Affiche cette aide et ignore les autres options."
+    echo "  <id_centrale>  (optionnel) Filtre les résultats pour une centrale spécifique."
+    echo "  <num_station>  (optionnel) Numéro de la station à traiter (doit être un entier positif)."
+    echo
+    echo "Exemple d'utilisation :"
+    echo "  $0 -h"
+    echo "  $0 12345"
+    echo "  $0 12345 5"
+    exit 0
+}
+
+# On verifie si l'un des parametre est -h
+for argument in "$*"; do
+    if [[ "$argument" == "-h" ]]; then
+        affiche_-h
+    fi
+done
+
+
+
 # On verifie le nombre de parametre
 if [ "$#" -lt 3 ]; then
     echo "Erreur: Parametre manquant."
@@ -26,8 +54,8 @@ if [[ "$2" != "hvb" && "$2" != "hva" && "$2" != "lv" ]]; then
     exit 3
 fi
 
-if [[ "$3" != "hvb" && "$3" != "hva" && "$3" != "lv" ]]; then
-    echo "Erreur: Le deuxieme parametre n'est pas 'hub','hva' ou 'lv'."
+if [[ "$3" != "comp" && "$3" != "indiv" && "$3" != "all" ]]; then
+    echo "Erreur: Le deuxieme parametre n'est pas 'comp','indiv' ou 'all'."
     exit 4
 fi
 
@@ -45,8 +73,15 @@ if [[ "$2" == "hva" && ( "$3" == "all" || "$3" == "indiv" ) ]]; then
 fi
 
 # On verifie si il y a le 4eme parametre et qu'il est possible
-if [ ! -n "$4"];then
-    if [ "$4" gt 0]; then
+
+if [ ! -z "$4" ];then
+    # On verifie que le parametre est un int
+    if ! [[ "$4" =~ ^[0-9]+$ ]]; then
+        echo "Erreur: Le numéro de la station doit être un entier."
+        exit 7
+    fi
+    # On verifie qu'il est positif
+    if [ "$4" -lt 1 ]; then
         echo "Erreur: Le numero de la station doit etre positif."
         exit 7
     fi
