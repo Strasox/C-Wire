4ddf2675aa2fdf2d9143a3b3f93b117e0f12e74d
Strasox <133694962+Strasox@users.noreply.github.com>
2024-12-04 13:09:35 +0000

    Quelque ajout dans le shell

--- c-wire.sh
+++ c-wire.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+if [ "$#" -lt 3 ]; then
+    echo "Erreur: Paramètre manquant."
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo "Erreur: Le fichiers de donnée n'existe pas ou n'a pas été trouvée."
+    exit 1
+fi
+
