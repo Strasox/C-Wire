cd0ff89e332dfffe92049585a77537d8e545a9c4
AlexisG115 <131152071+AlexisG115@users.noreply.github.com>
2024-12-11 22:46:27 +0100

    Update temp.c

--- Programme/codeC/temp.c
+++ Programme/codeC/temp.c
@@ -1,2 +1,25 @@
 #include <stdio.h>
 #include <stlib.h>
+
+typedef struct avl_struct
+{
+    int val;             // La valeur du nœud
+    int eq;                // Facteur d'équilibre (balance factor)
+    struct avl_struct *fg; // Pointeur vers le fils gauche
+    struct avl_struct *fd; // Pointeur vers le fils droit
+} AVL;
+
+AVL* creerAVL(int e)
+{
+    // Alloue de la mémoire pour un nouveau nœud
+    AVL* new = (AVL* )malloc(sizeof(AVL));
+    if (new == NULL)
+    {
+        exit(EXIT_FAILURE); // Arrêt immédiat en cas d'erreur d'allocation
+    }
+    new->val = e; // Initialisation de la valeur
+    new->fg = NULL; // Pas de fils gauche
+    new->fd = NULL; // Pas de fils droit
+    new->eq = 0;    // Facteur d'équilibre initialisé à 0
+    return new;
+}
