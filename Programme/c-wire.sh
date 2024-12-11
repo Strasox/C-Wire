aa7fbcdefe4fc1233c0272ce19ab7d038da67059
Strasox <133694962+Strasox@users.noreply.github.com>
2024-12-11 20:35:54 +0000

    fini de verifier les parametre parfait

--- Programme/c-wire.sh
+++ Programme/c-wire.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-## On verifie les argument entrer par l'utilisateur
-
 # Fonction pour afficher l'aide (-h)
 affiche_-h(){
-    echo
-    echo
     echo "Aide : $0 [-h] <chemin_du_fichier> <type_de_station> <type_de_consommateur> [Identifiant_de_centrale]"
     echo
     echo "Description :"
     echo "Ce script permet de traiter les informations d'un fichier selon les paramètre de l'utilisateur de crée un nouveau fichier avec ces résultat."
-    echo
+    echo  
     echo "Options :"
     echo " [-h] (Optionnel) Affiche l'aide de cette commande et ignore les autres options."
     echo " <chemin_du_fichier> (Obligatoire) Chemin indiquant l'endroit du fichier à traiter."
@@ -20,70 +16,66 @@ affiche_-h(){
     echo
     echo "Limitation :"
     echo " Les options (hvb, hva) ne sont pas compatible avec (all, indiv) et ne sont donc pas autoriser."
-    exit 0
+    echo "--------------------"
 }
+
+## On verifie les argument entrer par l'utilisateur
 
 # On verifie si l'un des parametre est -h
 for option in "$@"; do
     if [[ "$option" == "-h" ]]; then
         affiche_-h
+        exit 0
     fi
 done
 
-
-
 # On verifie le nombre de parametre
 if [ "$#" -lt 3 ]; then
+    affiche_-h
     echo "Erreur: Parametre manquant."
     exit 1
 fi
 
-# On verifie que le 1er parametre est un fichier et donc si il existe.
+# On verifie que le 1er parametre est un fichier et si on le trouve.
 if [ ! -f "$1" ]; then
-    echo "Erreur: La premier parametre n'est pas un fichier ou n'existe pas."
+    affiche_-h
+    echo "Erreur: Le premier parametre n'est pas un fichier ou est introuvable."
     exit 2
 fi
 
-
-### Piste amélioration : posibilite de faire un case ?
-
-
 # On verifie que le 2eme parametre est 'hub','hva' ou 'lv'
-
-
 if [[ "$2" != "hvb" && "$2" != "hva" && "$2" != "lv" ]]; then
-    echo "Erreur: Le deuxieme parametre n'est pas 'hub','hva' ou 'lv'."
+    affiche_-h
+    echo "Erreur: Le parametre type de station n'est pas 'hub','hva' ou 'lv' et n'est donc pas valide."
     exit 3
 fi
 
+# On verifie que le 3eme parametre est 'comp','indiv' ou 'all'
 if [[ "$3" != "comp" && "$3" != "indiv" && "$3" != "all" ]]; then
-    echo "Erreur: Le deuxieme parametre n'est pas 'comp','indiv' ou 'all'."
+    affiche_-h
+    echo "Erreur: Le parametre type de consomateur n'est pas 'comp','indiv' ou 'all' et n'est donc pas valide."
     exit 4
 fi
 
-
-
-# On verifie que le 3eme parametre n'est pas interdite
-if [[ "$2" == "hvb" && ( "$3" == "all" || "$3" == "indiv" ) ]]; then
-    echo "Erreur: Le troisieme parametre ne peut pas etre all ou indiv avec hvb."
+# On verifie que les 2eme et 3eme parametre sont compatible
+if [[ ("$2" == "hvb" || "$2" == "hva") && ( "$3" == "all" || "$3" == "indiv" ) ]]; then
+    affiche_-h
+    echo "Erreur: Le parametre type de station et type de consomateur ne sont pas compatible."
     exit 5
 fi
 
-if [[ "$2" == "hva" && ( "$3" == "all" || "$3" == "indiv" ) ]]; then
-    echo "Erreur: Le troisieme parametre ne peut pas etre all ou indiv avec hva."
-    exit 6
-fi
-
-# On verifie si il y a le 4eme parametre et qu'il est possible
-
+# On verifie si le numéro de station est en parametre
 if [ ! -z "$4" ];then
-    # On verifie que le parametre est un int
-    if ! [[ "$4" =~ ^[0-9]+$ ]]; then
+    # On verifie que le parametre est un int (StackOverflow)
+    if ! [[ "$4" =~ ^[0-9]+$ ]]; then 
+        affiche_-h
         echo "Erreur: Le numéro de la station doit être un entier."
         exit 7
     fi
-    # On verifie qu'il est positif
+
+    # On verifie qu'il est supperieur ou egal a 1
     if [ "$4" -lt 1 ]; then
+        affiche_-h
         echo "Erreur: Le numero de la station doit etre positif."
         exit 7
     fi
