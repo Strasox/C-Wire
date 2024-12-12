23e84b2d67818c26fe2c26dadbabbee75e97e031
AlexisG115 <131152071+AlexisG115@users.noreply.github.com>
2024-12-12 23:16:11 +0100

    Update temp.c

--- codeC/temp.c
+++ codeC/temp.c
@@ -15,13 +15,13 @@ typedef struct station
 
 typedef struct avl_struct
 {
-    Station *val;             // La valeur du nœud
-    int eq;                // Facteur d'équilibre (balance factor)
+    Station *val;             // pointeur vers les valeurs du nœud
+    int eq;                // Facteur d'équilibre 
     struct avl_struct *fg; // Pointeur vers le fils gauche
     struct avl_struct *fd; // Pointeur vers le fils droit
 } AVL;
 
-AVL* creerAVL(int e)
+AVL* creerAVL(int e, int f, int g, int h, int i, int j, int k)
 {
     // Alloue de la mémoire pour un nouveau nœud
     AVL* new = (AVL* )malloc(sizeof(AVL));
@@ -29,43 +29,17 @@ AVL* creerAVL(int e)
     {
         exit(EXIT_FAILURE); // Arrêt immédiat en cas d'erreur d'allocation
     }
-    new->val = e; // Initialisation de la valeur
+    new->val->centrale = e;
+    new->val->hvb = f;
+    new->val->hva = g;
+    new->val->lv = h;
+    new->val->compagnie = i;
+    new->val->capacite = j;
+    new->val->conso = k;
     new->fg = NULL; // Pas de fils gauche
     new->fd = NULL; // Pas de fils droit
     new->eq = 0;    // Facteur d'équilibre initialisé à 0
     return new;
-}
-
-AVL* insertionAVL(AVL* a, int e, int *h)
-{
-    if (a == NULL)
-    {           // Si l'arbre est vide, crée un nouveau nœud
-        *h = 1; // La hauteur a augmenté
-        return creerAVL(e);
-    }
-    else if (e < a->val)
-    { // Si l'élément est plus petit, insérer à gauche
-        a->fg = insertionAVL(a->fg, e, h);
-        *h = -*h; // Inverse l'impact de la hauteur
-    }
-    else if (e > a->val)
-    { // Si l'élément est plus grand, insérer à droite
-        a->fd = insertionAVL(a->fd, e, h);
-    }
-    else
-    { // Élément déjà présent
-        *h = 0;
-        return a;
-    }
-
-    // Mise à jour du facteur d'équilibre et rééquilibrage si nécessaire
-    if (*h != 0)
-    {
-        a->eq += *h;
-        a = equilibrerAVL(a);
-        *h = (a->eq == 0) ? 0 : 1; // Mise à jour de la hauteur
-    }
-    return a;
 }
 
 AVL* equilibrerAVL(AVL* a)
@@ -93,5 +67,134 @@ AVL* equilibrerAVL(AVL* a)
         }
     }
     return a; // Aucun rééquilibrage nécessaire
+}
+
+
+AVL* insertionAVL(AVL* a, int k, int *h)
+{
+    if (a == NULL)
+    {           // Si l'arbre est vide, crée un nouveau nœud
+        *h = 1; // La hauteur a augmenté
+        return creerAVL(k);
+    }
+    else if (k < a->val->conso)
+    { // Si consommation est inférieure, insérer à gauche
+        a->fg = insertionAVL(a->fg, k, h);
+        *h = -*h; // Inverse l'impact de la hauteur
+    }
+    else if (k > a->val->conso)
+    { // Si la consommation est plus grande, insérer à droite
+        a->fd = insertionAVL(a->fd, k, h);
+    }
+    else
+    { // Élément déjà présent
+        *h = 0;
+        return a;
+    }
+
+    // Mise à jour du facteur d'équilibre et rééquilibrage si nécessaire
+    if (*h != 0)
+    {
+        a->eq += *h;
+        a = equilibrerAVL(a);
+        *h = (a->eq == 0) ? 0 : 1; // Mise à jour de la hauteur
+    }
+    return a;
+}
+
+int sommeConsoHvb(AVL *a, AVL *b, int *somme) //retourne la somme de la consommation de la station 'a'
+{
+    if (a == NULL)
+    {
+        return 0;
+    }
+    if (b->val->centrale != a->val->centrale)
+    {
+        return 0;
+    }
+    if (a->val->hvb != NULL && a->val->hvb == b->val->hvb)
+    {
+        somme += b->val->conso; //somme des grandes entreprises
+        if (b->fg != NULL)
+        {
+            b->fg = sommeConso(a, b->fg, somme);
+        }
+        if (b->fd != NULL)
+        {
+            b->fd = sommeConso(a, b->fd , somme);
+        }
+        return somme;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int sommeConsoHva(AVL *a, AVL *b, int *somme) //retourne la somme de la consommation de la station 'a'
+{
+    if (a == NULL)
+    {
+        return 0;
+    }
+    if (b->val->centrale != a->val->centrale)
+    {
+        return 0;
+    }
+    if (a->val->hvb != NULL && a->val->hvb == b->val->hvb)
+    {
+        if ( a->val->hva != NULL && a->val->hva == b->val->hva)
+        {
+            somme += b->val->conso; //somme de conso 
+            if (b->fg != NULL)
+            {
+                b->fg = sommeConso(a, b->fg, somme);
+            }
+            if (b->fd != NULL)
+            {
+                b->fd = sommeConso(a, b->fd , somme);
+            }
+            return somme;
+        }
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int sommeConsoLv(AVL *a, AVL *b, int *somme) //retourne la somme de la consommation de la station 'a'
+{
+    if (a == NULL)
+    {
+        return 0;
+    }
+    if (b->val->centrale != a->val->centrale)
+    {
+        return 0;
+    }
+    if (a->val->hvb != NULL && a->val->hvb == b->val->hvb)
+    {
+        if ( a->val->hva != NULL && a->val->hva == b->val->hva) //test si la station a existe et si b est issu de la même station que a
+        {
+            if ( a->val->lv != NULL && a->val->lv == b->val->lv )
+            {
+                somme += b->val->conso; //somme de conso 
+                if (b->fg != NULL)
+                {
+                    b->fg = sommeConso(a, b->fg, somme);
+                }
+                if (b->fd != NULL)
+                {
+                    b->fd = sommeConso(a, b->fd , somme);
+                }
+                return somme;
+            }
+        }
+    }
+    else
+    {
+        return 0;
+    }
 }
 
