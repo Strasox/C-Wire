23d83baa4970ae90e9cf602c31703ed85cf18d3f
Strasox <133694962+Strasox@users.noreply.github.com>
2024-12-13 15:38:04 +0000

    makefile temporaire pour compiler tout le code

--- c-wire.sh
+++ c-wire.sh
@@ -103,14 +103,24 @@ if [ ! -z "$4" ];then
 fi
 
 # Verification de l'existance de l'executable c 
-#if [ ! -x "codeC/exec" ]; then
-#    make -C codeC
-#    if [ ! -x "codeC/exec" ]; then
-#        echo "Erreu: Le fichier executable C est introuvable et ne peut pas etre cree."
-#        affiche_temps
-#        exit 8
-#    fi
-#fi
+if [ ! -x "codeC/exec" ]; then
+    # Verification de l'existance du makefile
+    if (! make -C codeC); then
+        echo "Erreur: Le makefile n'existe pas."
+        affiche_temps
+        exit 8
+    fi
+
+    # Execution du makefile
+    make -C codeC --no-print-directory
+
+    # Verification de l'existance de l'executable c 
+    if [ ! -x "codeC/exec" ]; then
+        echo "Erreu: Le fichier executable C est introuvable et ne peut pas etre cree."
+        affiche_temps
+        exit 9
+    fi
+fi
 
 # Vérification présence du dossier tmp et création si il n'existe pas 
 if [ ! -d "tmp" ]; then 
