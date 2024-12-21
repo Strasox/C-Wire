8bd9dd522976493ff2034188de60a3b00d75bde4
Strasox <133694962+Strasox@users.noreply.github.com>
2024-12-21 17:51:43 +0100

    Update fonctions.h

--- codeC/fonctions.h
+++ codeC/fonctions.h
@@ -1,12 +1,10 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "structure.h"
-
 
 // Fonction pour obtenir le maximum
 int max(int a, int b);
@@ -14,6 +12,7 @@ int max(int a, int b);
 // Fonction pour obtenir le minimum 
 int min(int a, int b);
 
+// Créé un oeud AVL avec une station
 AVL* creerAVL(Station* s);
 
 // Rotation gauche pour rééquilibrer un AVL
@@ -26,30 +25,31 @@ AVL* doubleRotationDroite(AVL* abr);
 
 AVL* doubleRotationGauche(AVL* abr);
 
+// On rééquilibre l'AVL selon l'équilibre des noeud
 AVL* equilibrerAVL(AVL* abr);
 
+// On insert une station dans l'AVL
 AVL* insertionAVL(AVL* abr,Station* s,int* h);
 
-// Suppression du minimum
+// Suppression du minimum de l'AVL
 AVL* suppMinAVL(AVL* abr, int* h, Station** stationMin);
 
-// Suppression
-AVL* suppressionAVL(AVL* abr, int id, int* h);
+// Fonction pour insérer chaque ligne du fichier des stations dans l'AVL
+void insertionStation(AVL** abr, int colonneStation);
 
-void insertionStation(AVL **abr, int colonneStation);
-
-//Recherche dans un AVL une station et modifie dans sa structure la consommation de la station
+// Recherche dans un AVL une station et modifie dans sa structure la consommation de la station
 AVL* modifierAVL(AVL* abr, int idStation, int somme);
 
-//Calcul la consommation d'un type de station en ddonnant la somme de tout les consommateurs
+// Calcul la consommation d'un type de station en donnant la somme de tout les consommateurs
 void calculConso(AVL* abr, int colonneStation);
 
+// Création du fichier avec les résultat de l'AVL
 void creerFichier(AVL* abr,char* s,char* c,char* central);
 
 // Affichage de l'AVL (ChatGPT pour vérifier si l'AVL fonctionne)
 void afficherAVL(AVL* a, int niveau);
 
-void interface(char *argv[]);
-
+// Programme principale
+void programme(char* argv[]);
 
 #endif
