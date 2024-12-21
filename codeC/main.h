b8eed2c645558efc4d84ab0453dd9cd48ff908f6
Strasox <133694962+Strasox@users.noreply.github.com>
2024-12-21 17:44:32 +0100

    Update main.h

--- codeC/main.h
+++ codeC/main.h
@@ -1,16 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "fonctions.h"
 #include "structure.h"
 
-
-
-
-int main();
+int main(int argc, char *argv[]);
 
 #endif
